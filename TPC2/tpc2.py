875487d788ed9af92e334b1c7395477cf85c0487
jpgoncalves13 <a95019@alunos.uminho.pt>
2023-02-26 23:25:00 +0000

    Geraçao das paginas html

---
+++ TPC2/tpc2.py
@@ -0,0 +1,80 @@
+import json
+
+def ordCidade(cidade):
+    return cidade["nome"]
+
+f = open("mapa.json")
+
+mapa = json.load(f)
+
+cidades = mapa["cidades"]
+cidades.sort(key=ordCidade)
+
+cidades_aux = dict()
+ligacoes = mapa["ligações"]
+
+index = """
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Mapa Virtual</title>
+        <meta charset="utf-8"/>
+    </head>
+    <body>
+        <center>
+            <h1>Mapa Virtual</h1>
+        </center>
+        <table>
+            <tr>
+                <!-- Índice-->
+                <td valign="top" width=30%>
+                    <a name="indice"/>
+                    <h3>Índice</h3>
+                    <ul>
+"""
+
+for c in cidades:
+    cidades_aux[c["id"]] = c
+    index += f"""
+    <li>
+        <a href="#{c["id"]}">{c["nome"]}</a>
+    </li>
+    """
+
+
+
+
+open("index.html", "w").write(index)
+i = 0
+
+pagHTML = """"""
+
+for c in cidades:
+    pagHTML += f"""
+                    <a name="{c["id"]}"/>
+                    <h3>{c["nome"]}</h3>
+                    <p><b>População: </b>{c["população"]}</p>
+                    <p><b>Descricção: </b>{c["descrição"]}</p>
+                    <p><b>Distrito: </b>{c["distrito"]}</p>
+                    <p><b>Ligações: </b>
+                        <ul>
+        """
+
+    for l in ligacoes:
+        if l["origem"] == c["id"]:
+            pagHTML += f"""
+                                <li>
+                                    <a href="#{l["destino"]}">{cidades_aux[l["destino"]]["nome"]}</a> - {l["distância"]} km
+                                </li>
+            """
+        elif l["destino"] == c["id"]:
+            pagHTML += f"""
+                                <li>
+                                    <a href="#{l["origem"]}">{cidades_aux[l["origem"]]["nome"]}</a> - {l["distância"]} km
+                                </li>
+            """
+    i = i+1
+    wr = "c" + str(i) + ".html"
+    open(wr,"w").write(pagHTML)
+    pagHTML=""
+
