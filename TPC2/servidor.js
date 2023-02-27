0fe600e9c59d7fef20de12914e16422b7cf84bd6
João Pedro Gonçalves <72882679+jpgoncalves13@users.noreply.github.com>
2023-02-27 22:52:26 +0000

    Delete servidor.js

--- TPC2/servidor.js
+++
@@ -1,8 +0,0 @@
-var http = require('http')
-var url = require('url')
-var fs = require('fs')
-
-var myServer = http.createServer(function(req, res) {
-    console.log(req.method + " " + req.url)
-    var pathname = req.
-})
