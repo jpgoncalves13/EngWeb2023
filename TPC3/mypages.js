ea46624731a71032c2c2c4c202e9356f1ca78712
jpgoncalves13 <a95019@alunos.uminho.pt>
2023-03-09 17:33:06 +0000

    so falta links

---
+++ TPC3/mypages.js
@@ -0,0 +1,282 @@
+// mypages.js
+// 2023-03-03 by gui
+// HTML templates generating function
+
+exports.genMainPage = function(lista, data){
+    var pagHTML = `
+    <!DOCTYPE html>
+
+    <html>
+        <head>
+            <meta charset="UTF-8"/>
+            <title>About People...</title>
+            <link rel="stylesheet" type="text/css" href="w3.css"/>
+        </head>
+        
+        <body>
+            <div class="w3-card-4">
+
+                <header class="w3-container w3-purple">
+                    <h1>Lista de pessoas</h1>
+                </header>
+
+                <div class="w3-container">
+                    <table class="w3-table-all">
+                    <tr>
+                        <th>ID</th>
+
+                        <th>Nome</th>
+
+                        <th>Idade</th>
+
+                        <th>Sexo</th>
+
+                        <th>Cidade</th>
+
+                        <th>Desportos Praticados</th>
+
+                        <th>Profissão</th>
+                    </tr>
+
+                `
+
+    for(let i = 0; i < lista.length; i++){
+        pagHTML += `
+        <tr>
+            <td>
+                <a href="/pessoas/${lista[i].id}"><td>${lista[i].nome}</a>
+            </td>
+            <td>${lista[i].idade}</td>
+
+            <td>${lista[i].sexo}</td>
+
+            <td>${lista[i].morada.cidade}</td>
+
+            <td>${lista[i].desportos}</td>
+
+            <td>${lista[i].profissao}</td>
+        </tr>
+        `
+
+    }
+
+    pagHTML += `
+                    </table>
+                </div>
+
+                    <footer class="w3-container w3-blue">
+                        <h5>Generated in EngWeb2023 ${data}</h5>
+                    </footer>
+
+                </div>
+            </body>
+        </html>
+        `
+
+    return pagHTML
+}
+
+exports.genPersonPage = function(p, d){
+    var pagHTML = `
+    <!DOCTYPE html>
+
+    <html>
+        <head>
+            <meta charset="UTF-8"/>
+            <title>Person Page...</title>
+            <link rel="stylesheet" type="text/css" href="w3.css"/>
+        </head>
+        
+        <body>
+            <div class="w3-card-4">
+
+                <header class="w3-container w3-purple">
+                    <h1>${p.nome}</h1>
+                </header>
+            <div class"container">
+                <p>Preencher com os outros campos</p>
+            </div>
+
+                <footer class="w3-container w3-blue">
+                    <h5>Generated in EngWeb2023 ${d}</h5>
+                 </footer>
+
+            </div>
+    </body>
+    </html>
+
+        `
+    return pagHTML
+}
+
+exports.genDistSexPage = function(dist, d){
+    pagHTML =`
+    <!DOCTYPE html>
+
+    <html>
+        <head>
+            <meta charset="UTF-8"/>
+            <title>Distribuição de Género</title>
+            <link rel="stylesheet" type="text/css" href="w3.css"/>
+        </head>
+
+        <body>
+            <div class="w3-card-4">
+
+                <header class="w3-container w3-purple">
+                    <h1>Distribuição de Género</h1>
+                </header>
+                
+                <div class="w3-container">
+                    <table class="w3-table-all">
+                        <tr>
+                            <th>Sexo</th>
+                            <th>Frequência</th>
+                        </tr>
+    `
+    for(let genre in dist){
+        pagHTML += `
+        <tr>
+            <td>
+            ${genre}
+            </td>
+
+            <td>
+            ${dist[genre]}
+            </td>
+        </tr>
+        `
+    }
+
+    pagHTML += `
+                    </table>
+                </div>
+
+                <footer class="w3-container w3-blue">
+                    <h5>Generated in EngWeb2023 ${d}</h5>
+                </footer>
+
+            </div>
+        </body>
+    </html>
+    `
+
+    return pagHTML
+}
+
+exports.genDistSport = function(dist, d){
+    pagHTML =`
+    <!DOCTYPE html>
+
+    <html>
+        <head>
+            <meta charset="UTF-8"/>
+            <title>Distribuição de Desporto</title>
+            <link rel="stylesheet" type="text/css" href="w3.css"/>
+        </head>
+
+        <body>
+            <div class="w3-card-4">
+
+                <header class="w3-container w3-purple">
+                    <h1>Distribuição de Desporto</h1>
+                </header>
+                
+                <div class="w3-container">
+                    <table class="w3-table-all">
+                        <tr>
+                            <th>Desporto</th>
+                            <th>Frequência</th>
+                        </tr>
+    `
+    var keys = Object.keys(dist).sort()
+    for(let i = 0; i < keys.length; i++){
+        pagHTML += `
+        <tr>
+            <td>
+            ${keys[i]}
+            </td>
+
+            <td>
+            ${dist[keys[i]]}
+            </td>
+        </tr>
+        `
+    }
+
+    pagHTML += `
+                    </table>
+                </div>
+
+                <footer class="w3-container w3-blue">
+                    <h5>Generated in EngWeb2023 ${d}</h5>
+                </footer>
+
+            </div>
+        </body>
+    </html>
+    `
+
+    return pagHTML
+}
+
+exports.genDistProfissao = function(dist, d) {
+    pagHTML =`
+    <!DOCTYPE html>
+
+    <html>
+        <head>
+            <meta charset="UTF-8"/>
+            <title>Top 10 Profissões</title>
+            <link rel="stylesheet" type="text/css" href="w3.css"/>
+        </head>
+
+        <body>
+            <div class="w3-card-4">
+
+                <header class="w3-container w3-purple">
+                    <h1>Top 10 Profissões</h1>
+                </header>
+                
+                <div class="w3-container">
+                    <table class="w3-table-all">
+                        <tr>
+                            <th>Desporto</th>
+                            <th>Frequência</th>
+                        </tr>
+    `
+    var values = Object.entries(dist).sort((a, b) => b[1] - a[1]).reduce((acc, [chave, valor]) => ({ ...acc, [chave]: valor }), {})
+    
+    let top10 = Object.fromEntries(Object.entries(values).slice(0,10))
+
+    for(let key in top10){
+
+        pagHTML += `
+        <tr>
+            <td>
+            ${key}
+            </td>
+
+            <td>
+            ${top10[key]}
+            </td>
+        </tr>
+        `
+    }
+
+    pagHTML += `
+                    </table>
+                </div>
+
+                <footer class="w3-container w3-blue">
+                    <h5>Generated in EngWeb2023 ${d}</h5>
+                </footer>
+
+            </div>
+        </body>
+    </html>
+    `
+
+    return pagHTML
+}
+
---
+++ TPC3/pessoas-server.js
@@ -0,0 +1,165 @@
+var http = require('http')
+var axios = require('axios')
+var mypages = require('./mypages')
+var fs = require('fs')
+
+function distSexo(pessoas){
+    dist = {}
+
+    for(let i = 0; i < pessoas.length; i++){
+        if(!(pessoas[i].sexo in dist)){
+            dist[pessoas[i].sexo] = 1
+        }
+        else{
+            dist[pessoas[i].sexo] += 1
+        }
+    }
+
+    return dist
+}
+
+function distDesporto(pessoas){
+    dist={}
+    for(let i = 0; i < pessoas.length; i++){
+        for(let l = 0; l < pessoas[i].desportos.length; l++){
+            if(!(pessoas[i].desportos[l] in dist)){
+                dist[pessoas[i].desportos[l]] = 1
+            }
+            else{
+                dist[pessoas[i].desportos[l]] += 1
+            }
+        }
+    }   
+    return dist
+}
+
+function distProfissao(pessoas){
+    dist={}
+    for(let i = 0; i < pessoas.length; i++){
+        if (!(pessoas[i].profissao in dist)) {
+            dist[pessoas[i].profissao] = 1
+        }
+        else {
+            dist[pessoas[i].profissao] += 1
+        }
+    }
+    return dist
+}
+
+var myServer = http.createServer(function (req,res) {
+    var d = new Date().toISOString().substring(0, 16);
+    console.log(req.method + " " + req.url + " " + d)
+
+    if(req.url == '/pessoas'){
+        axios.get('http://localhost:3000/pessoas')
+            .then(function(resp) { // especie de callback para sucesso
+                var pessoas = resp.data
+                res.writeHead(200, {'Content-Type' : 'text/html; charset=utf-8'})
+                res.end(mypages.genMainPage(pessoas, d))
+            })
+            .catch(erro => { // especie de callback para erro, equivalente a ter function(erro)
+                console.log("Erro: " + erro)
+                res.writeHead(200, {'Content-Type' : 'text/html; charset=utf-8'})
+                res.end('<p>Erro na obtençao de dados' + erro + '</p>')
+            })
+    }
+    else if(req.url == '/pessoasOrdenadas'){
+        axios.get("http://localhost:3000/pessoas?_sort=nome")
+            .then(function(resp){
+                var pessoas = resp.data
+                res.writeHead(200, {"Content-Type": "text/html; charset=utf-8"})
+                res.write(mypages.genMainPage(pessoas, d))
+                res.end()
+             })
+            .catch(erro => { 
+                console.log("Erro: " + erro)
+                res.writeHead(200, {"Content-Type": "text/html; charset=utf-8"})
+                res.end("<p>Erro na obtenção de dados: " + erro + "</p>")
+            })
+    }
+    else if (req.url == '/distSexo') {
+        axios.get("http://localhost:3000/pessoas")
+            .then(function(resp){
+                var pessoas = resp.data
+                dist = distSexo(pessoas)
+
+                res.writeHead(200, {"Content-Type": "text/html; charset=utf-8"})
+                res.write(mypages.genDistSexPage(dist, d))
+                res.end()
+             })
+            .catch(erro => { 
+                console.log("Erro: " + erro)
+                res.writeHead(200, {"Content-Type": "text/html; charset=utf-8"})
+                res.end("<p>Erro na obtenção de dados: " + erro + "</p>")
+            })
+    }
+    else if (req.url == '/distDesporto') {
+        axios.get("http://localhost:3000/pessoas")
+            .then(function(resp){
+                var pessoas = resp.data
+                dist = distDesporto(pessoas)
+
+                res.writeHead(200, {"Content-Type": "text/html; charset=utf-8"})
+                res.write(mypages.genDistSport(dist, d))
+                res.end()
+             })
+            .catch(erro => { 
+                console.log("Erro: " + erro)
+                res.writeHead(200, {"Content-Type": "text/html; charset=utf-8"})
+                res.end("<p>Erro na obtenção de dados: " + erro + "</p>")
+            })
+    }
+    else if (req.url == '/topProfissoes') {
+        axios.get("http://localhost:3000/pessoas")
+            .then(function(resp){
+                var pessoas = resp.data
+                dist = distProfissao(pessoas)
+
+                res.writeHead(200, {"Content-Type": "text/html; charset=utf-8"})
+                res.write(mypages.genDistProfissao(dist, d))
+                res.end()
+             })
+            .catch(erro => { 
+                console.log("Erro: " + erro)
+                res.writeHead(200, {"Content-Type": "text/html; charset=utf-8"})
+                res.end("<p>Erro na obtenção de dados: " + erro + "</p>")
+            })
+    }
+
+    else if(req.url.match(/w3\.css$/)){
+        fs.readFile("w3.css", function(erro, dados){        
+            if(erro){
+                res.writeHead(200, {'Content-Type' : 'text/html; charset=utf-8'})
+                res.end("<p>ERRO: na leitura do ficheiro :: " + erro + "</p>") // dentro pra n fechar o pacote antes de ler o ficheiro
+            }
+            else {
+                res.writeHead(200, {'Content-Type' : 'text/css'})
+                res.end(dados) // dentro pra n fechar o pacote antes de ler o ficheiro
+            }
+    
+        })
+    }  
+    else if(req.url.match(/\/pessoas\/p\d+/)){
+        console.log('Pedindo ' + req.url.substring(9))
+        axios.get('http://localhost:3000/pessoas/' + req.url.substring(9))
+            .then(function(resp) { // especie de callback para sucesso
+                var pessoa = resp.data
+                res.writeHead(200, {'Content-Type' : 'text/html; charset=utf-8'})
+                res.write(mypages.genPersonPage(pessoa, d))
+                res.end()
+            })
+            .catch(erro => { // especie de callback para erro, equivalente a ter function(erro)
+                console.log("Erro: " + erro)
+                res.writeHead(200, {'Content-Type' : 'text/html; charset=utf-8'})
+                res.end('<p>Erro na obtençao de dados' + erro + '</p>')
+            })
+    }
+    else{
+        res.writeHead(404, {'Content-Type' : 'text/html; charset=utf-8'})
+        res.end('<p>Operação não suportada: ' + req.url + '</p>')
+    }
+
+}).listen(7778)
+
+
+console.log("Servidor à escuta na porta 7778...")
